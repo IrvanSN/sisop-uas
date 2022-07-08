d4d1cd6c3440659053656eedd2bb6a619d6c8920
Ubuntu <ubuntu@ip-172-31-22-236.ap-southeast-1.compute.internal>
2022-07-08 18:42:13 +0000

    fix nomersatu

--- nomersatu.sh
+++ nomersatu.sh
@@ -1,3 +1,33 @@
-#! /bin/bash
+#!/bin/bash
 # program nomer satu
 
+echo -n "Masukkan angka yang ingin anda hitung: "
+read -a equation
+
+a=${equation[0]}
+b=${equation[1]}
+c=${equation[2]}
+d=${equation[3]}
+e=${equation[4]}
+
+if [[ $b == ":" ]]
+then
+	b="/"
+fi
+
+if [[ $b == x ]]
+then
+	b="*"
+fi
+
+if [[ $d == ":" ]]
+then
+	d="/"
+fi
+
+if [[ $d == x ]]
+then
+	d="*"
+fi
+
+echo $(($a$b$c$d$e))
